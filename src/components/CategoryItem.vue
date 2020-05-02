8e63fa54daf7dfd92e63c03b8ee5aa22a4075b31
Kaikaine <kairijb@protonmail.com>
2020-05-01 23:29:22 -0500

    grab categories from DB

---
+++ src/components/CategoryItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <div :key="category._id" class="column is-one-quarter" :style="{'min-height': '160px'}">
+    <a href="#">
+      <span class="is-primary is-top is-medium tooltip">
+        <figure class="image is-4by3 imageFade">
+          <img class="is-rounded" :src="category.image" alt="Image of cateogry" />
+        </figure>
+        <div class="subtitle m-t-xs bold">{{category.name | capitalize}}</div>
+      </span>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CategoryItem",
+  props: {
+    category: {
+      required: true,
+      type: Object
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.is-rounded {
+  border-radius: 10px !important;
+}
+</style>
