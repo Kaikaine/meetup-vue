5185737133889659cbf7094518c9cb6ddd64dab9
Kaikaine <kairijb@protonmail.com>
2020-05-01 20:16:30 -0500

    init commit with server and npm packages

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,54 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <TheNavbar />
+    <PageHome />
+    <TheFooter />
   </div>
 </template>
 
+<script>
+import PageHome from "@/pages/PageHome";
+import TheNavbar from "@/components/shared/TheNavbar";
+import TheFooter from "@/components/shared/TheFooter";
+export default {
+  name: "app",
+  components: {
+    PageHome,
+    TheNavbar,
+    TheFooter
+  }
+};
+</script>
+
 <style lang="scss">
+@import "assets/css/spacing.css";
+@import "~bulma/bulma.sass";
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.bold {
+  font-weight: bold;
+}
+.cover {
+  background-size: cover;
+  /*background-position: center;*/
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+}
+.hero {
+  position: relative;
+}
+.hero-body {
+  padding: 3rem 1.5rem;
+}
+.hero-bg {
+  background-image: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),
+    url("https://images.unsplash.com/photo-1531263060782-b024de9b9793?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import AppDropdown from "./components/shared/AppDropdown";
+import AppHero from "./components/shared/AppHero";
 
 Vue.config.productionTip = false;
 
+Vue.component("AppHero", AppHero);
+Vue.component("AppDropdown", AppDropdown);
+
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
