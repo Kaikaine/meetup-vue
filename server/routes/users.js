498c78b824c38908e67967e250014ea302edbe96
Kaikaine <kairijb@protonmail.com>
2020-05-03 08:51:03 -0500

    authenticate middleware

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const UsersCtrl = require("../controllers/users");
-router.post("/register", UsersCtrl.register);
-router.post("/login", UsersCtrl.register);
 
 router.get("", UsersCtrl.getUsers);
+
+router.post("/register", UsersCtrl.register);
+router.post("/login", UsersCtrl.login);
+router.post("/logout", UsersCtrl.logout);
 
 module.exports = router;
